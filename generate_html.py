71737d94dc51980d145cdcaf8b956077dc2c3dbe
Nicole0101 <33416598+Nicole0101@users.noreply.github.com>
2026-04-10 00:23:32 +0800

    Update generate_html.py

--- generate_html.py
+++ generate_html.py
@@ -60,7 +60,7 @@ def get_TWSE_data():
 
 # ===== 讀CSV =====
 def load_stock_list():
-    df = pd.read_csv("NextStocks.csv", sep="\t", encoding="utf-8-sig")
+    df = pd.read_csv("NextStocks.CSV", sep="\t", encoding="utf-8-sig")
     df = df.rename(columns={"Ticker": "stock_id", "Name": "name"})
     return df.to_dict(orient="records")
 

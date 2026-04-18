d1a92a62b931398a8571314ce644efba64a3e78a
Nicole0101 <33416598+Nicole0101@users.noreply.github.com>
2026-04-18 23:31:50 +0800

    Update line_push.py

--- line_push.py
+++ line_push.py
@@ -24,6 +24,7 @@ def send_line(msg):
         }
 
         res = requests.post(url, headers=headers, json=data)
+        print("LINE status:", res.status_code, res.text)
 
     except Exception as e:
         print("LINE error:", e)
